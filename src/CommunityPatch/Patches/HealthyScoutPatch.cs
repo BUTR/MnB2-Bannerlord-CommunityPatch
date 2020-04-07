3e5816f3b27a53228f16d9d9efc43ea77c8eac15
Tyler Young <tyler.young@impromptu.ninja>
2020-04-07 17:18:29 -0400

    implementation via model wrapping

--- src/CommunityPatch/Patches/HealthyScoutPatch.cs
+++ src/CommunityPatch/Patches/HealthyScoutPatch.cs
@@ -1,7 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Helpers;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
+using static CommunityPatch.HarmonyHelpers;
+using Harmony = HarmonyLib.Harmony;
 
 namespace CommunityPatch.Patches {
 
@@ -12,10 +20,52 @@ namespace CommunityPatch.Patches {
     private readonly PerkObject _perk
       = PerkObject.FindFirst(x => x.Name.GetID() == "dDKOoD3e");
 
+    private static readonly Type CharacterStatsModelType = typeof(CharacterStatsModel);
+
+    private static readonly Type DefaultCharacterStatsModelType = typeof(DefaultCharacterStatsModel);
+
+    private CharacterStatsModel _modelToWrap;
+
+    private WrapperCharacterStatsModel _wrapped;
+
+    private class WrapperCharacterStatsModel : CharacterStatsModel {
+
+      private CharacterStatsModel _wrapped;
+
+      private HealthyScoutPatch _patch;
+
+      public WrapperCharacterStatsModel(CharacterStatsModel wrapped, HealthyScoutPatch patch) {
+        _wrapped = wrapped;
+        _patch = patch;
+      }
+
+      public override int MaxHitpoints(CharacterObject character, StatExplainer explanation = null) {
+        var result = _wrapped.MaxHitpoints(character, explanation);
+
+        var explainedNumber = new ExplainedNumber(result, explanation, null);
+
+        PerkHelper.AddPerkBonusForCharacter(_patch._perk, character, ref explainedNumber);
+
+        return MBMath.Round(explainedNumber.ResultNumber);
+      }
+
+    }
+
     public bool IsApplicable(Game game)
       // ReSharper disable once CompareOfFloatsByEqualityOperator
-      => _perk.PrimaryRole == SkillEffect.PerkRole.PartyMember
-        && _perk.PrimaryBonus == 0.15f;
+    {
+      if (!(_perk.PrimaryRole == SkillEffect.PerkRole.PartyMember
+        && _perk.PrimaryBonus == 0.15f))
+        return false;
+
+      var models = (List<GameModel>) CommunityPatchSubModule.CampaignGameStarter.Models;
+      _modelToWrap = (CharacterStatsModel) models.Find(model => CharacterStatsModelType.IsInstanceOfType(model));
+      var modelToWrapType = _modelToWrap.GetType();
+
+      // check if model is already non-default
+      return modelToWrapType == DefaultCharacterStatsModelType
+        || (modelToWrapType.Namespace?.StartsWith(nameof(CommunityPatch)) ?? false);
+    }
 
     public void Apply(Game game) {
       // Dear TaleWorlds; Value should probably be publicly exposed, maybe by a method
@@ -38,6 +88,14 @@ namespace CommunityPatch.Patches {
         _perk.SecondaryRole, _perk.SecondaryBonus,
         _perk.IncrementType
       );
+
+      var models = (List<GameModel>) CommunityPatchSubModule.CampaignGameStarter.Models;
+
+      _wrapped = new WrapperCharacterStatsModel(_modelToWrap, this);
+
+      models.Remove(_modelToWrap);
+      models.Add(_wrapped);
+
       Applied = true;
     }
 
--- src/CommunityPatch/Patches/PeakFormPatch.cs
+++ src/CommunityPatch/Patches/PeakFormPatch.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using Helpers;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 
 namespace CommunityPatch.Patches {
@@ -12,9 +16,51 @@ namespace CommunityPatch.Patches {
     private readonly PerkObject _perk
       = PerkObject.FindFirst(x => x.Name.GetID() == "fBgGbxaw");
 
+    private static readonly Type CharacterStatsModelType = typeof(CharacterStatsModel);
+
+    private static readonly Type DefaultCharacterStatsModelType = typeof(DefaultCharacterStatsModel);
+
+    private CharacterStatsModel _modelToWrap;
+
+    private WrapperCharacterStatsModel _wrapped;
+
+    private class WrapperCharacterStatsModel : CharacterStatsModel {
+
+      private CharacterStatsModel _wrapped;
+
+      private PeakFormPatch _patch;
+
+      public WrapperCharacterStatsModel(CharacterStatsModel wrapped, PeakFormPatch patch) {
+        _wrapped = wrapped;
+        _patch = patch;
+      }
+
+      public override int MaxHitpoints(CharacterObject character, StatExplainer explanation = null) {
+        var result = _wrapped.MaxHitpoints(character, explanation);
+
+        var explainedNumber = new ExplainedNumber(result, explanation, null);
+
+        PerkHelper.AddPerkBonusForCharacter(_patch._perk, character, ref explainedNumber);
+
+        return MBMath.Round(explainedNumber.ResultNumber);
+      }
+
+    }
+
     public bool IsApplicable(Game game)
       // ReSharper disable once CompareOfFloatsByEqualityOperator
-      => _perk.PrimaryBonus == 0f;
+    {
+      if (_perk.PrimaryBonus != 0f)
+        return false;
+
+      var models = (List<GameModel>) CommunityPatchSubModule.CampaignGameStarter.Models;
+      _modelToWrap = (CharacterStatsModel) models.Find(model => CharacterStatsModelType.IsInstanceOfType(model));
+      var modelToWrapType = _modelToWrap.GetType();
+
+      // check if model is already non-default
+      return modelToWrapType == DefaultCharacterStatsModelType
+        || (modelToWrapType.Namespace?.StartsWith(nameof(CommunityPatch)) ?? false);
+    }
 
     public void Apply(Game game) {
       // Dear TaleWorlds; Value should probably be publicly exposed, maybe by a method
@@ -37,6 +83,14 @@ namespace CommunityPatch.Patches {
         _perk.SecondaryRole, _perk.SecondaryBonus,
         _perk.IncrementType
       );
+
+      var models = (List<GameModel>) CommunityPatchSubModule.CampaignGameStarter.Models;
+
+      _wrapped = new WrapperCharacterStatsModel(_modelToWrap, this);
+
+      models.Remove(_modelToWrap);
+      models.Add(_wrapped);
+
       Applied = true;
     }
 
