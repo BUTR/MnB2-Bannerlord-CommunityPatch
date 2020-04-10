6177052b73b88da533408094af5ca742a6ecbefa
Adam <admsvy@gmail.com>
2020-04-10 18:28:55 -0400

    Split patches and implement interface

---
+++ src/CommunityPatch/Patches/CrossbowCrossbowCavalryPatch.cs
@@ -0,0 +1,33 @@
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace CommunityPatch.Patches {
+
+  public class CrossbowCrossbowCavalryPatch : AgentWeaponEquippedPatch {
+    
+    private static PerkObject CrossbowCavalry => PerkObject.FindFirst(perk => perk.Name.GetID() == "sHXLjzCb");
+
+    protected override bool _IsApplicable(Game game)
+      => CommunityPatchSubModule.VersionComparer.GreaterThan(CommunityPatchSubModule.GameVersion, ApplicationVersion.FromString("e1.0.0"));
+
+    protected override void Apply(Agent __instance,
+      EquipmentIndex equipmentSlot,
+      ref WeaponData weaponData,
+      ref WeaponStatsData[] weaponStatsData,
+      ref WeaponData ammoWeaponData,
+      ref WeaponStatsData[] ammoWeaponStatsData,
+      GameEntity weaponEntity) {
+      for (int i = 0; i < weaponStatsData.Length; i++) {
+        var weapon = weaponStatsData[i];
+        if (weapon.WeaponClass == (int) WeaponClass.Crossbow && HeroHasPerk(__instance.Character, CrossbowCavalry)) {
+          var updatedWeapon = weapon;
+          updatedWeapon.WeaponFlags = weapon.WeaponFlags & ~((ulong) WeaponFlags.CantReloadOnHorseback);
+          weaponStatsData[i] = updatedWeapon;
+        }
+      }
+    }
+  }
+}
