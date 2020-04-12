24531c9cdf4af463f430a1859e43f35f1e8535bb
Tyler Young <tyler.young@impromptu.ninja>
2020-04-12 15:54:20 -0400

    fix double-hashed output of GeneratePatchTargetHashes
    fix updated hashes
    
    add debugging define DEBUG_METHOD_SIGNATURE checks in MethodHelpers.MakeCilSignatureSha256 around source recording code

--- src/CommunityPatch/MethodHelpers.cs
+++ src/CommunityPatch/MethodHelpers.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
@@ -15,6 +18,11 @@ namespace CommunityPatch {
       .GetField("offset", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
 
     public static byte[] MakeCilSignatureSha256(this MethodBase mb) {
+#if DEBUG_METHOD_SIGNATURE
+      using var hashSource = new MemoryStream(65536);
+      var hashSourceStrs = new LinkedList<string>();
+#endif
+
       var il = PatchProcessor.ReadMethodBody(mb);
       var buf = new byte[32];
       using var hasher = SHA256.Create();
@@ -24,6 +32,9 @@ namespace CommunityPatch {
         fixed (byte* pBuf = buf)
           *(ulong*) pBuf = v;
         hasher.TransformBlock(buf, 0, size, null, 0);
+#if DEBUG_METHOD_SIGNATURE
+        hashSource.Write(buf, 0, size);
+#endif
       }
 
       void ProcessSigned(long v, int size)
@@ -31,24 +42,37 @@ namespace CommunityPatch {
 
       void ProcessString(string s) {
         using var sigHasher = SHA256.Create();
-        var callSig = sigHasher.ComputeHash(Encoding.Unicode.GetBytes(s));
-        hasher.TransformBlock(callSig, 0, 32, null, 0);
+        var sig = sigHasher.ComputeHash(Encoding.Unicode.GetBytes(s));
+        hasher.TransformBlock(sig, 0, 32, null, 0);
+#if DEBUG_METHOD_SIGNATURE
+        hashSourceStrs.AddLast(s);
+        hashSource.Write(sig, 0, 32);
+#endif
       }
 
       unsafe void ProcessSingle(float v) {
         fixed (byte* pBuf = buf)
           *(float*) pBuf = v;
         hasher.TransformBlock(buf, 0, 4, null, 0);
+#if DEBUG_METHOD_SIGNATURE
+        hashSource.Write(buf, 0, 4);
+#endif
       }
 
       unsafe void ProcessDouble(double v) {
         fixed (byte* pBuf = buf)
           *(double*) pBuf = v;
         hasher.TransformBlock(buf, 0, 8, null, 0);
+#if DEBUG_METHOD_SIGNATURE
+        hashSource.Write(buf, 0, 8);
+#endif
       }
 
       void ProcessInstrRef(object r) {
         ProcessSigned((int) IlInstrOffsetField.GetValue(r), 4);
+#if DEBUG_METHOD_SIGNATURE
+        hashSource.Write(buf, 0, 4);
+#endif
       }
 
       ProcessString(mb.FullDescription());
@@ -63,6 +87,9 @@ namespace CommunityPatch {
         }
 
         hasher.TransformBlock(buf, 0, opCode.Size, null, 0);
+#if DEBUG_METHOD_SIGNATURE
+        hashSource.Write(buf, 0, opCode.Size);
+#endif
 
         switch (operand) {
           // @formatter:off
@@ -119,6 +146,35 @@ namespace CommunityPatch {
       }
 
       hasher.TransformFinalBlock(buf, 0, 0);
+
+#if DEBUG_METHOD_SIGNATURE
+      {
+        var hashSb = new StringBuilder(64);
+        for (var i = 0; i < 32; ++i)
+          hashSb.AppendFormat($"{hasher.Hash[i]:X2}");
+        var hashStr = hashSb.ToString();
+
+        var sb = new StringBuilder();
+        hashSource.Position = 0;
+        for (;;) {
+          var b = hashSource.ReadByte();
+          if (b == -1) break;
+
+          sb.AppendFormat($"{b:X2}");
+        }
+
+        CommunityPatchSubModule.Print("=============================================");
+        CommunityPatchSubModule.Print("============== Source Data: =================");
+        CommunityPatchSubModule.Print(sb.ToString());
+        CommunityPatchSubModule.Print("============ Source Strings: ================");
+        foreach (var s in hashSourceStrs)
+          CommunityPatchSubModule.Print(s);
+        CommunityPatchSubModule.Print("=============================================");
+        CommunityPatchSubModule.Print("Hash: " + hashStr);
+        CommunityPatchSubModule.Print("=============================================");
+        CommunityPatchSubModule.Print("=============================================");
+      }
+#endif
 
       return hasher.Hash;
     }
