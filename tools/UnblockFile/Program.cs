862fd8465713076ad3d9f13a30c34df799ecd0ca
Tyler Young <tyler.young@impromptu.ninja>
2020-04-13 19:58:00 -0400

    add a home grown unblock file implementation that doesn't copy from anybody's stuff so everybody can copy it instead

---
+++ tools/UnblockFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Com;
+
+namespace UnblockFile {
+
+  class Program {
+
+    static void Main(string[] args) {
+
+      foreach (var fileName in args) {
+        var pzi = new PersistentZoneIdentifier();
+        pzi.Load(fileName, (int) StorageMode.Read);
+        pzi.Remove();
+      }
+    }
+
+  }
+
+}
