00858fec42efeac8a9a1bd0c7f90a3a83d78543b
fnzr <5471818+fnzr@users.noreply.github.com>
2020-04-17 00:29:01 -0300

    Fix hidden Smithing Perk #141

---
+++ src/CommunityPatch/Patches/Perks/Endurance/Smithing/SiegeExpertHiddenPatch.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace CommunityPatch.Patches.Perks.Endurance.Smithing {
+
+  [HarmonyPatch(typeof(DefaultPerks), "InitializePerks")]
+  public class SiegeExpertHiddenPerkPatch {
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Postfix(DefaultPerks __instance) {
+      var siegeExpert = typeof(DefaultPerks).GetField("CraftingSiegeExpert", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+      var legendarySmith = typeof(DefaultPerks).GetField("CraftingLegendarySmith",BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+      
+      var alternativePerk = typeof(PerkObject).GetProperty("AlternativePerk", BindingFlags.Instance | BindingFlags.Public);
+      alternativePerk.SetValue(siegeExpert, legendarySmith);
+      alternativePerk.SetValue(legendarySmith, siegeExpert);
+    }
+  }
+}
