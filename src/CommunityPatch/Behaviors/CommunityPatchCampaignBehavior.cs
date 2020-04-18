40d272bfa6c11dc48d26e3fcb3ba83161efc971d
fnzr <5471818+fnzr@users.noreply.github.com>
2020-04-18 19:24:28 -0300

    Add CampaignBehavior to force party speed refresh

---
+++ src/CommunityPatch/Behaviors/CommunityPatchCampaignBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+
+namespace CommunityPatch.Behaviors {
+
+  public class CommunityPatchCampaignBehavior : CampaignBehaviorBase {
+
+    public override void RegisterEvents()
+    {
+      CampaignEvents.PerkOpenedEvent.AddNonSerializedListener(this, OnPerkOpened);
+    }
+
+    private void OnPerkOpened(Hero hero, PerkObject openedPerk)
+    {
+      if (hero != null && openedPerk == DefaultPerks.Riding.NomadicTraditions) {
+        var versionField = typeof(MobileParty).GetField("_partyPureSpeedLastCheckVersion", BindingFlags.Instance | BindingFlags.NonPublic);
+        versionField.SetValue(hero.PartyBelongedTo, -1);
+      }
+    }
+
+    public override void SyncData(IDataStore dataStore)
+    {
+    }
+  }
+}
