144050839f5440ad19641fe8bc89c90520aba93e
fnzr <5471818+fnzr@users.noreply.github.com>
2020-04-20 13:41:21 -0300

    Remove explicit magic numbers, add hashes

--- src/CommunityPatch/Patches/Perks/Endurance/Riding/NomadicTraditionsPatch.cs
+++ src/CommunityPatch/Patches/Perks/Endurance/Riding/NomadicTraditionsPatch.cs
@@ -10,52 +10,52 @@ using static CommunityPatch.HarmonyHelpers;
 
 namespace CommunityPatch.Patches.Perks.Endurance.Riding {
 
-  public sealed class NomadicTraditionsPatch : PatchBase<NomadicTraditionsPatch> {
+  public sealed partial class NomadicTraditionsPatch : PatchBase<NomadicTraditionsPatch> {
 
     public override bool Applied { get; protected set; }
 
-    private static readonly MethodInfo TargetMethodInfo = typeof(DefaultPartySpeedCalculatingModel).GetMethod("CalculatePureSpeed", BindingFlags.Public | BindingFlags.Instance);
+    private static readonly MethodInfo PureSpeedMethodInfo = typeof(DefaultPartySpeedCalculatingModel).GetMethod("CalculatePureSpeed", BindingFlags.Public | BindingFlags.Instance);
+    
+    private static readonly MethodInfo CavalryRatioModifierMethodInfo = typeof(DefaultPartySpeedCalculatingModel).GetMethod("GetCavalryRatioModifier", BindingFlags.NonPublic | BindingFlags.Instance);
+    
+    private static readonly MethodInfo MountedFootmenRatioModifierMethodInfo = typeof(DefaultPartySpeedCalculatingModel).GetMethod("GetMountedFootmenRatioModifier", BindingFlags.NonPublic | BindingFlags.Instance);
+    
+    private static readonly MethodInfo BaseSpeedMethodInfo = typeof(DefaultPartySpeedCalculatingModel).GetMethod("CalculateBaseSpeedForParty", BindingFlags.Instance | BindingFlags.NonPublic);
 
     private static readonly MethodInfo PatchMethodInfo = typeof(NomadicTraditionsPatch).GetMethod(nameof(Postfix), BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
 
+    private PerkObject _perk;
+
     public override IEnumerable<MethodBase> GetMethodsChecked() {
-      yield return TargetMethodInfo;
+      yield return PureSpeedMethodInfo;
+      yield return CavalryRatioModifierMethodInfo;
+      yield return MountedFootmenRatioModifierMethodInfo;
+      yield return BaseSpeedMethodInfo;
     }
 
-    private static readonly byte[][] Hashes = {
-      new byte[] {
-        // e1.2.0.226040
-        0x61, 0xD7, 0x4D, 0xF5, 0xB0, 0x0E, 0x84, 0x52,
-        0xDC, 0xCB, 0x2F, 0xE7, 0xE2, 0x20, 0x38, 0x10,
-        0x87, 0x01, 0xE3, 0x61, 0xF1, 0xAB, 0x89, 0x7D,
-        0x9C, 0xDC, 0x50, 0x6E, 0xA6, 0x7E, 0xEB, 0xEF
-      }
-    };
-
-    public override void Reset() {}
+    public override void Reset() =>
+      _perk = PerkObject.FindFirst(x => x.Name.GetID() == "PB5iowxh");
 
     public override bool? IsApplicable(Game game)
     {
-      var patchInfo = Harmony.GetPatchInfo(TargetMethodInfo);
+      var patchInfo = Harmony.GetPatchInfo(PureSpeedMethodInfo);
       if (AlreadyPatchedByOthers(patchInfo))
         return false;
 
-      var hash = TargetMethodInfo.MakeCilSignatureSha256();
-      return hash.MatchesAnySha256(Hashes);
+      return HashesMatch();
     }
 
     public override void Apply(Game game) {
       if (Applied) return;
 
-      CommunityPatchSubModule.Harmony.Patch(TargetMethodInfo,
+      CommunityPatchSubModule.Harmony.Patch(PureSpeedMethodInfo,
         postfix: new HarmonyMethod(PatchMethodInfo));
       Applied = true;
     }
 
-    private static float GetPerkBaseRatio(float magicNumber) {
-      var perkBonus = DefaultPerks.Riding.NomadicTraditions.PrimaryBonus;
-      return (magicNumber * (1 + perkBonus)) - magicNumber;
-    }
+    private static float GetPerkBaseRatio(float magicNumber) =>
+      (magicNumber * (1 + ActivePatch._perk.PrimaryBonus)) - magicNumber;
+    
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void Postfix(DefaultPartySpeedCalculatingModel __instance, ref StatExplainer explanation, ref float __result, MobileParty mobileParty, ref int additionalTroopOnFootCount, ref int additionalTroopOnHorseCount) {
@@ -77,17 +77,20 @@ namespace CommunityPatch.Patches.Perks.Endurance.Riding {
         }
         float mountedFootman = Math.Min(availableHorses, troopsOnFoot);
         var mountedFootmanRatio = totalTroops == 0 ? 0 : mountedFootman / totalTroops;
-        var perkMountedFootmanRatio = GetPerkBaseRatio(0.3f) * mountedFootmanRatio;
+        var mountedFootmanMagicNumber = (float)MountedFootmenRatioModifierMethodInfo.Invoke(__instance, new object[] {1, 1});
+        var perkMountedFootmanRatio = GetPerkBaseRatio(mountedFootmanMagicNumber) * mountedFootmanRatio;
         
         var cavalryRatio = totalTroops == 0 ? 0 : mountedTroops / totalTroops;
-        var perkCavalryRatio = GetPerkBaseRatio(0.6f) * cavalryRatio;
-        
-        var baseSpeedMethod = typeof(DefaultPartySpeedCalculatingModel).GetMethod("CalculateBaseSpeedForParty", BindingFlags.Instance | BindingFlags.NonPublic);
-        var baseSpeed = (float)baseSpeedMethod.Invoke(__instance, new object[] { totalTroops });
+        var cavalryMagicNumber = (float)CavalryRatioModifierMethodInfo.Invoke(__instance, new object[] {1, 1});
+        var perkCavalryRatio = GetPerkBaseRatio(cavalryMagicNumber) * cavalryRatio;
 
-        var perkBonus = (perkCavalryRatio + perkMountedFootmanRatio) * baseSpeed;
+        var baseSpeed = (float)BaseSpeedMethodInfo.Invoke(__instance, new object[] {totalTroops});
+        
+        var explainedNumber = new ExplainedNumber(baseSpeed, explanation);
+        explainedNumber.AddFactor(perkCavalryRatio, ActivePatch._perk.Name);
+        explainedNumber.AddFactor(perkMountedFootmanRatio, ActivePatch._perk.Name);
+        var perkBonus = explainedNumber.ResultNumber - baseSpeed;
         __result += perkBonus;
-        explanation?.AddLine("Nomadic Traditions", perkBonus, StatExplainer.OperationType.Add);
       }
     }
   }
