f36cf4e63e0096519cc149f1099f18187f1f0219
wonkotron <wonkotron@gmail.com>
2020-04-20 23:08:20 -0700

    fix has value, complete IsApplicable, code cleanup

--- src/CommunityPatch/Patches/Feats/KhuzaitCavalryAgilityPatch.cs
+++ src/CommunityPatch/Patches/Feats/KhuzaitCavalryAgilityPatch.cs
@@ -1,14 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
-using Helpers;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 using TaleWorlds.Core;
-using TaleWorlds.Localization;
 
 namespace CommunityPatch.Patches.Feats {
     public sealed class KhuzaitCavalryAgilityPatch : PatchBase<KhuzaitCavalryAgilityPatch> {
@@ -23,17 +20,18 @@ namespace CommunityPatch.Patches.Feats {
 
         private static readonly byte[][] ValidHashes = {
             new byte[] {
-                // e1.1.0.224785
-                0x58, 0xF5, 0x64, 0xA2, 0xE1, 0x17, 0x5C, 0x0C,
-                0x13, 0xEE, 0xED, 0xA8, 0x5E, 0xFC, 0xDF, 0x38,
-                0x7E, 0xCE, 0x1E, 0xF9, 0xF8, 0x67, 0x78, 0x3F,
-                0xB9, 0x71, 0xE0, 0x02, 0x8D, 0x58, 0x40, 0x99
+                // e1.2.0.226271 and presumably previous versions
+                0x61, 0xD7, 0x4D, 0xF5, 0xB0, 0x0E, 0x84, 0x52,
+                0xDC, 0xCB, 0x2F, 0xE7, 0xE2, 0x20, 0x38, 0x10,
+                0x87, 0x01, 0xE3, 0x61, 0xF1, 0xAB, 0x89, 0x7D,
+                0x9C, 0xDC, 0x50, 0x6E, 0xA6, 0x7E, 0xEB, 0xEF
             }
         };
         
         public override bool? IsApplicable(Game game) {
-            // Currently ignores if method patched by others, expecting that there is a postfix applied
-            return true;
+            // Currently ignores if method patched by others, expecting that there is a postfix already applied
+            var hash = PureSpeedMethodInfo.MakeCilSignatureSha256();
+            return hash.MatchesAnySha256(ValidHashes);
         }
 
         public override void Apply(Game game) {
@@ -61,8 +59,8 @@ namespace CommunityPatch.Patches.Feats {
                 mobileParty.Leader.GetFeatValue(DefaultFeats.Cultural.KhuzaitCavalryAgility)) {
 
                 // Get private methods needed for calculation
-                var GetCavalryRatioModifierMethod = AccessTools.Method(__instance.GetType(), "GetCavalryRatioModifier");
-                var GetMountedFootmenRatioModifierMethod = AccessTools.Method(__instance.GetType(), "GetMountedFootmenRatioModifier");
+                var getCavalryRatioModifierMethod = AccessTools.Method(__instance.GetType(), "GetCavalryRatioModifier");
+                var getMountedFootmenRatioModifierMethod = AccessTools.Method(__instance.GetType(), "GetMountedFootmenRatioModifier");
 
                 // Recalculate Cavalry and Footmen on horses speed modifiers based on 
                 int menCount = mobileParty.MemberRoster.TotalManCount + additionalTroopOnFootCount +
@@ -82,10 +80,10 @@ namespace CommunityPatch.Patches.Feats {
                     }
                 }
 
-                int min_footmenCount_numberOfAvailableMounts = Math.Min(footmenCount, numberOfAvailableMounts);
+                int minFootmenCountNumberOfAvailableMounts = Math.Min(footmenCount, numberOfAvailableMounts);
 
-                float cavalryRatioModifier = (float) GetCavalryRatioModifierMethod.Invoke(__instance, new object[] {menCount, horsemenCount});
-                float mountedFootmenRatioModifier = (float) GetMountedFootmenRatioModifierMethod.Invoke(__instance, new object[] {menCount, min_footmenCount_numberOfAvailableMounts});
+                float cavalryRatioModifier = (float) getCavalryRatioModifierMethod.Invoke(__instance, new object[] {menCount, horsemenCount});
+                float mountedFootmenRatioModifier = (float) getMountedFootmenRatioModifierMethod.Invoke(__instance, new object[] {menCount, minFootmenCountNumberOfAvailableMounts});
 
                 // calculate Khuzait bonus and apply
                 float khuzaitRatioModifier = DefaultFeats.Cultural.KhuzaitCavalryAgility.EffectBonus *
