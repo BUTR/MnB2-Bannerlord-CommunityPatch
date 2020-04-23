84c5e7d16d9a1a560d7843c204ee2c1139690759
Tyler Young <tyler.young@impromptu.ninja>
2020-04-23 00:56:16 -0400

    defer refs to StoryMode assembly
    switch NextHourlyTickListener for StrongBox<Action>
    
    different logic for missing constant; assume patch no longer applicable

--- src/CommunityPatch/Patches/EarlyStoryVisibleTimeoutPatch.cs
+++ src/CommunityPatch/Patches/EarlyStoryVisibleTimeoutPatch.cs
@@ -2,9 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using StoryMode;
-using StoryMode.Behaviors;
-using StoryMode.StoryModePhases;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.Localization;
@@ -12,100 +11,100 @@ using TaleWorlds.Localization;
 namespace CommunityPatch.Patches {
 
   public sealed class EarlyStoryVisibleTimeoutPatch : IPatch {
-    // default if we can't obtain vanilla's value
-    private const int DefaultFirstPhaseTimeLimitInYears = 10;
-    private readonly int FirstPhaseTimeLimitInYears;
 
-    public EarlyStoryVisibleTimeoutPatch() {
-      FirstPhaseTimeLimitInYears = ExtractFirstPhaseTimeLimitInYears();
-    }
+    private static readonly Type FirstPhaseCampaignBehaviorType = Type.GetType("StoryMode.Behaviors.FirstPhaseCampaignBehavior, StoryMode, Version=1.0.0.0, Culture=neutral");
+
+    private static readonly Type FirstPhaseType = Type.GetType("StoryMode.StoryModePhases.FirstPhase, StoryMode, Version=1.0.0.0, Culture=neutral");
+
+    private static readonly MethodInfo FirstPhaseInstanceGetter = AccessTools.PropertyGetter(FirstPhaseType, "Instance");
+
+    private static object FirstPhaseInstance => FirstPhaseInstanceGetter.Invoke(null, null);
+
+    private static readonly MethodInfo FirstPhaseFirstPhaseStartTimeGetter = AccessTools.PropertyGetter(FirstPhaseType, "FirstPhaseStartTime");
+
+    private static CampaignTime GetFirstPhaseFirstPhaseStartTime(object instance) => (CampaignTime) FirstPhaseFirstPhaseStartTimeGetter.Invoke(instance, null);
+
+    private static readonly Type SecondPhaseType = Type.GetType("StoryMode.StoryModePhases.SecondPhase, StoryMode, Version=1.0.0.0, Culture=neutral");
+
+    private static readonly MethodInfo SecondPhaseInstanceGetter = AccessTools.PropertyGetter(SecondPhaseType, "Instance");
+
+    private static object SecondPhaseInstance => SecondPhaseInstanceGetter.Invoke(null, null);
+
+    private static readonly int FirstPhaseTimeLimitInYears = ExtractFirstPhaseTimeLimitInYears();
+
+    private static readonly FieldInfo FirstPhaseTimeLimitInYearsConst = FirstPhaseCampaignBehaviorType.GetField("FirstPhaseTimeLimitInYears", BindingFlags.NonPublic | BindingFlags.Static);
 
     public bool Applied { get; private set; }
 
     public void Reset() {
-      if (Applied) {
-        CampaignEvents.OnQuestStartedEvent.ClearListeners(this);
-        Applied = false;
-      }
+      if (!Applied)
+        return;
+
+      CampaignEvents.OnQuestStartedEvent.ClearListeners(this);
+      Applied = false;
     }
 
-    public bool? IsApplicable(Game game) => game.GameType is StoryMode.CampaignStoryMode;
+    // if they remove the const, they probably got rid of the time limit
+    public bool? IsApplicable(Game game)
+      => FirstPhaseTimeLimitInYearsConst != null
+        && game.GameType.GetType().FullName == "StoryMode.CampaignStoryMode";
 
     public IEnumerable<MethodBase> GetMethodsChecked() {
       yield break;
     }
 
-    private static int ExtractFirstPhaseTimeLimitInYears() {
-      try {
-        FieldInfo field = typeof(FirstPhaseCampaignBehavior).GetField("FirstPhaseTimeLimitInYears", BindingFlags.NonPublic | BindingFlags.Static);
-        return (int)field.GetRawConstantValue();
-      }
-      catch (NullReferenceException ex) {
-        // couldn't locate the field in vanilla. Maybe the name has changed, or it's access modifiers.
-        CommunityPatchSubModule.Error(ex, $"{typeof(EarlyStoryVisibleTimeoutPatch).Name}: Couldn't locate vanilla's timeout value." + Environment.NewLine);
-      }
-      return DefaultFirstPhaseTimeLimitInYears;
-    }
+    private static int ExtractFirstPhaseTimeLimitInYears()
+      => (int) FirstPhaseTimeLimitInYearsConst!.GetRawConstantValue();
 
     public void Apply(Game game) {
-      CampaignEvents.OnQuestStartedEvent.AddNonSerializedListener(
-        this,
-        new Action<QuestBase>(OnQuestStarted)
-      );
-      foreach (QuestBase quest
-               in Campaign.Current.QuestManager.Quests.ToList<QuestBase>()) {
+      CampaignEvents.OnQuestStartedEvent.AddNonSerializedListener(this, OnQuestStarted);
+
+      foreach (var quest in Campaign.Current.QuestManager.Quests.ToList())
         SetStoryVisibleTimeoutIfNeeded(quest);
-      }
 
       Applied = true;
     }
 
-    private static bool IsFirstStoryPhase() {
-      return FirstPhase.Instance != null
-               && SecondPhase.Instance == null;
-    }
+    private static bool IsFirstStoryPhase()
+      => FirstPhaseInstance != null
+        && SecondPhaseInstance == null;
 
-    private void SetStoryVisibleTimeoutIfNeeded(QuestBase quest) {
+    private static void SetStoryVisibleTimeoutIfNeeded(QuestBase quest) {
       if (!IsFirstStoryPhase() || !quest.IsSpecialQuest || !quest.IsOngoing)
         return;
 
       // set visible timeout to be when vanilla would have (silently) timed 
       // out the quest,  minus a day to make very sure the quest doesn't 
       // somehow trigger vanilla's silent timeout too early.
-      CampaignTime newDueTime = FirstPhase.Instance.FirstPhaseStartTime
-                                  + CampaignTime.Years(FirstPhaseTimeLimitInYears)
-                                  - CampaignTime.Days(1);
-      if (quest.QuestDueTime != newDueTime) {
-        quest.ChangeQuestDueTime(newDueTime);
-        ShowNotification(new TextObject("{=QuestTimeRemainingUpdated}Quest time remaining was updated."), "event:/ui/notification/quest_update");
-      }
+      var newDueTime = GetFirstPhaseFirstPhaseStartTime(FirstPhaseInstance)
+        + CampaignTime.Years(FirstPhaseTimeLimitInYears)
+        - CampaignTime.Days(1);
+      if (quest.QuestDueTime == newDueTime)
+        return;
+
+      quest.ChangeQuestDueTime(newDueTime);
+      ShowNotification(
+        new TextObject("{=QuestTimeRemainingUpdated}Quest time remaining was updated."),
+        "event:/ui/notification/quest_update"
+      );
     }
 
-    private static void ShowNotification(TextObject message, string soundEventPath = "") {
-      InformationManager.AddQuickInformation(message, 0, null, soundEventPath);
-    }
-
-    class NextHourlyTickListener {
-      private Action<QuestBase> _func;
-      private QuestBase _quest;
-
-      public NextHourlyTickListener(Action<QuestBase> func, QuestBase quest) {
-        _func = func;
-        _quest = quest;
-        CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, OnHourlyTick);
-      }
-
-      private void OnHourlyTick() {
-        _func.Invoke(_quest);
-        CampaignEvents.HourlyTickEvent.ClearListeners(this);
-      }
-    };
+    private static void ShowNotification(TextObject message, string soundEventPath = "")
+      => InformationManager.AddQuickInformation(message, 0, null, soundEventPath);
 
     private void OnQuestStarted(QuestBase quest) {
+      // give OnHourlyTick's closure a late bound self-ref
+      var onHourlyTickBoxed = new StrongBox<Action>();
+
       // defer our updates until some time has passed, because they depend on whether 
       // FirstPhase or SecondPhase is active, and story Phase information is only 
       // updated after all OnQuestStarted handlers have fired.
-      new NextHourlyTickListener(new Action<QuestBase>(SetStoryVisibleTimeoutIfNeeded), quest);
+      onHourlyTickBoxed.Value = () => {
+        SetStoryVisibleTimeoutIfNeeded(quest);
+        CampaignEvents.HourlyTickEvent.ClearListeners(onHourlyTickBoxed.Value);
+      };
+
+      CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, onHourlyTickBoxed.Value);
     }
 
   }
