0f763c71544c5135d59935a56f9c678e06891b39
Miguel <miguelcjalmeida@gmail.com>
2020-04-30 14:51:44 -0300

    add perk Logistics from tactics

---
+++ src/CommunityPatch/Patches/Perks/Cunning/Tactics/LogisticsPatch.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using static System.Reflection.BindingFlags;
+
+namespace CommunityPatch.Patches.Perks.Cunning.Tactics {
+
+  public class LogisticsPatch : ExtraAmmoPerksPatch<LogisticsPatch> {
+
+    private static readonly MethodInfo PatchMethodInfo = typeof(LogisticsPatch).GetMethod(nameof(Postfix), NonPublic | Static | DeclaredOnly);
+    
+    public override void Apply(Game game) {
+      if (Applied) return;
+      CommunityPatchSubModule.Harmony.Patch(TargetMethodInfo, postfix: new HarmonyMethod(PatchMethodInfo));
+      Applied = true;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void Postfix(Agent __instance) {
+      if (MyLeaderIsIntoLogistics(__instance)) 
+        ApplyPerkToAgent(__instance, CanApplyPerk, CalculateAmmoIncrease);
+    }
+
+    private static bool MyLeaderIsIntoLogistics(Agent agent) {
+      var leader = ((PartyGroupAgentOrigin) agent.Origin)?.Party.Leader;
+      if (leader == null || leader == agent.Character) return false;
+      return leader.GetPerkValue(DefaultPerks.Tactics.Logistics);
+    }
+
+    private static bool CanApplyPerk(Agent agent, WeaponComponentData weaponComponentData) {
+      var weaponClass = WeaponComponentData.GetItemTypeFromWeaponClass(weaponComponentData.WeaponClass);
+      
+      return weaponClass == ItemObject.ItemTypeEnum.Arrows ||
+        weaponClass == ItemObject.ItemTypeEnum.Bolts ||
+        weaponClass == ItemObject.ItemTypeEnum.Thrown;
+    }
+
+    private static int CalculateAmmoIncrease(short maxAmmo)
+      => (int) (maxAmmo * DefaultPerks.Tactics.Logistics.PrimaryBonus);
+  }
+
+}
