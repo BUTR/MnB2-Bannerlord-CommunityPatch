180a6e2f0b5ac646361ba405f1cd6a1e9bc9e844
Tyler Young <tyler.young@impromptu.ninja>
2020-05-01 14:18:13 -0400

    refactored BaitPatch for readability and reduced some expressiveness down to local vars

--- src/CommunityPatch/Patches/Perks/Cunning/Tactics/BaitPatch.cs
+++ src/CommunityPatch/Patches/Perks/Cunning/Tactics/BaitPatch.cs
@@ -16,10 +16,11 @@ namespace CommunityPatch.Patches.Perks.Cunning.Tactics {
   public class BaitPatch : PatchBase<BaitPatch> {
 
     public override bool Applied { get; protected set; }
-    
+
     private static readonly MethodInfo TargetMethodInfo = PlayerEncounterHelper.TargetMethodInfo;
+
     private static readonly MethodInfo PatchMethodInfo = typeof(BaitPatch).GetMethod(nameof(Postfix), Public | NonPublic | Static | DeclaredOnly);
-    
+
     public override IEnumerable<MethodBase> GetMethodsChecked() {
       yield return TargetMethodInfo;
     }
@@ -59,8 +60,9 @@ namespace CommunityPatch.Patches.Perks.Cunning.Tactics {
         _perk.SecondaryRole, _perk.SecondaryBonus,
         SkillEffect.EffectIncrementType.AddFactor
       );
-      
+
       if (Applied) return;
+
       CommunityPatchSubModule.Harmony.Patch(TargetMethodInfo, postfix: new HarmonyMethod(PatchMethodInfo));
       Applied = true;
     }
@@ -72,72 +74,109 @@ namespace CommunityPatch.Patches.Perks.Cunning.Tactics {
 
       AddNearbyAlliesToParty(ourParty, encounterParty, partiesToJoinOurSide);
       AddNearbyAlliesToParty(encounterParty, ourParty, partiesToJoinEnemySide, true);
-      
+
       partiesToJoinEnemySide = partiesToJoinEnemySide.Distinct().ToList();
       partiesToJoinOurSide = partiesToJoinOurSide.Distinct().ToList();
     }
 
-    private static void AddNearbyAlliesToParty(PartyBase party, PartyBase enemyParty, List<MobileParty> alliesFound,  bool allyMustBeAbleToAttack = false) {
+    private static void AddNearbyAlliesToParty(PartyBase party, PartyBase enemyParty, List<MobileParty> alliesFound, bool allyMustBeAbleToAttack = false) {
       var isSiege = IsSiegeEncounter();
       var position2D = GetEncounterPosition(isSiege);
       var radius = CalculateCallToArmsRadius(party, isSiege);
 
       var possibleAllies = PlayerEncounterHelper.FindPartiesAroundPosition(position2D, radius);
-      
+
       foreach (var possibleAlly in possibleAllies) {
-        if (!IsAlly(party, enemyParty, possibleAlly, allyMustBeAbleToAttack)) continue;
+        if (!IsAlly(party, enemyParty, possibleAlly, allyMustBeAbleToAttack))
+          continue;
 
         alliesFound.Add(possibleAlly);
-          
+
         if (possibleAlly.Army != null && possibleAlly.Army.LeaderParty == possibleAlly)
           alliesFound.AddRange(possibleAlly.Army.LeaderParty.AttachedParties);
       }
     }
-    
-    private static bool IsSiegeEncounter() 
-      => PlayerEncounter.EncounteredParty != null && 
-        PlayerEncounter.EncounteredParty.IsMobile && 
-        PlayerEncounter.EncounteredParty.MobileParty.BesiegedSettlement != null || 
+
+    private static bool IsSiegeEncounter()
+      => PlayerEncounter.EncounteredParty != null &&
+        PlayerEncounter.EncounteredParty.IsMobile &&
+        PlayerEncounter.EncounteredParty.MobileParty.BesiegedSettlement != null ||
         MobileParty.MainParty.BesiegedSettlement != null;
 
     private static Vec2 GetEncounterPosition(bool isSiegeEvent) {
       if (!isSiegeEvent) return MobileParty.MainParty.Position2D;
 
-      if (PlayerEncounter.EncounteredParty?.IsMobile == true && PlayerEncounter.EncounteredParty?.MobileParty?.BesiegedSettlement != null)
-      {
-        if (PlayerEncounter.EncounteredParty.SiegeEvent?.BesiegerCamp?.BesiegerParty != null)
-          return PlayerEncounter.EncounteredParty.SiegeEvent.BesiegerCamp.BesiegerParty.Position2D;
-      }
-      else if (MobileParty.MainParty.BesiegerCamp?.BesiegerParty != null)
-        return MobileParty.MainParty.BesiegerCamp.BesiegerParty.Position2D;
+      var encountered = PlayerEncounter.EncounteredParty;
 
-      return MobileParty.MainParty.Position2D;
+      if (encountered?.IsMobile == true && encountered?.MobileParty?.BesiegedSettlement != null) {
+        return encountered.SiegeEvent?.BesiegerCamp?.BesiegerParty?.Position2D
+          ?? MobileParty.MainParty.Position2D;
+      }
+
+      return MobileParty.MainParty.BesiegerCamp?.BesiegerParty?.Position2D
+        ?? MobileParty.MainParty.Position2D;
     }
-    
-    private static float CalculateCallToArmsRadius(PartyBase party, bool isSiege)
-    {
+
+    private static float CalculateCallToArmsRadius(PartyBase party, bool isSiege) {
       var baseRadius = 3f * (isSiege ? 1.5f : 1f);
       var finalRadius = new ExplainedNumber(baseRadius);
-      
+
       if (party.MobileParty != null)
         PerkHelper.AddPerkBonusForParty(ActivePatch._perk, party.MobileParty, ref finalRadius);
-      
+
       return finalRadius.ResultNumber;
     }
-    
-    private static bool IsAlly(PartyBase party, PartyBase enemyParty, MobileParty possibleAlly, bool allyMustBeAbleToAttack)
-    {
-      if (!IsAValidPossibleAlly(party, possibleAlly)) 
+
+    private static bool IsAlly(PartyBase party, PartyBase enemyParty, MobileParty possibleAlly, bool allyMustBeAbleToAttack) {
+      if (!IsAValidPossibleAlly(party, possibleAlly))
         return false;
-      
-      if (!MapEventHelper.PartyCanJoinSideOf(possibleAlly, party, enemyParty)) 
+
+      if (!MapEventHelper.PartyCanJoinSideOf(possibleAlly, party, enemyParty))
         return false;
 
       return !allyMustBeAbleToAttack || PlayerEncounterHelper.CanPartyAttack(possibleAlly, enemyParty.MobileParty);
     }
 
-    private static bool IsAValidPossibleAlly(PartyBase party, MobileParty possibleAlly)
-      => possibleAlly != MobileParty.MainParty && possibleAlly != PlayerEncounter.EncounteredParty.MobileParty && possibleAlly.MapEvent == null && possibleAlly.IsActive && (possibleAlly.BesiegedSettlement == null || (MobileParty.MainParty.MapEvent != null && MobileParty.MainParty.MapEvent.IsSiege && MobileParty.MainParty.MapEvent.MapEventSettlement == possibleAlly.BesiegedSettlement) || (MobileParty.MainParty.Army != null && MobileParty.MainParty.Army.LeaderParty.AttachedParties.Contains(possibleAlly))) && (possibleAlly.Army == null || !possibleAlly.Army.LeaderParty.AttachedParties.Contains(possibleAlly)) && (possibleAlly.CurrentSettlement == null || MobileParty.MainParty.BesiegedSettlement == possibleAlly.CurrentSettlement || (party.MobileParty != null && possibleAlly.CurrentSettlement == party.MobileParty.CurrentSettlement));
+    private static bool IsAValidPossibleAlly(PartyBase party, MobileParty possibleAlly) {
+      var mainParty = MobileParty.MainParty;
+      if (possibleAlly == mainParty
+        || possibleAlly == PlayerEncounter.EncounteredParty.MobileParty
+        || possibleAlly.MapEvent != null
+        || !possibleAlly.IsActive)
+        return false;
+
+      if (possibleAlly.BesiegedSettlement != null) {
+        var mapEvent = mainParty.MapEvent;
+
+        var allyInArmy = mainParty.Army?.LeaderParty.AttachedParties.Contains(possibleAlly) ?? false;
+
+        if (mapEvent != null) {
+          var mapEventType = mapEvent.EventType;
+          var isSiege = mapEventType != MapEvent.BattleTypes.Siege
+            && mapEventType != MapEvent.BattleTypes.SiegeOutside;
+
+          if (isSiege
+            || mapEvent.MapEventSettlement != possibleAlly.BesiegedSettlement
+            && allyInArmy)
+            return false;
+        }
+        else {
+          if (allyInArmy)
+            return false;
+        }
+      }
+
+      if (possibleAlly.Army?.LeaderParty.AttachedParties.Contains(possibleAlly) ?? false)
+        return false;
+
+      var allySettlement = possibleAlly.CurrentSettlement;
+
+      return allySettlement == null
+        || mainParty.BesiegedSettlement == allySettlement
+        || party.MobileParty != null
+        && allySettlement == party.MobileParty.CurrentSettlement;
+    }
+
   }
 
 }
