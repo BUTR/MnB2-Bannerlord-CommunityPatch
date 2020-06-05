c1352ddd864bd2f9f920023156ae7abbcabf4844
Joël Forward <jforward@live.fr>
2020-06-05 04:13:29 +0200

    skip patches when PatchObsolute/PatchNotBefore is used according to game version

--- src/CommunityPatch/PatchApplicabilityHelper.cs
+++ src/CommunityPatch/PatchApplicabilityHelper.cs
@@ -7,6 +7,29 @@ namespace CommunityPatch {
 
   public static class PatchApplicabilityHelper {
 
+    private static readonly ApplicationVersionComparer VersionComparer = new ApplicationVersionComparer();
+    
+    public static String SkippedPatchReason(IPatch patch) {
+      var reason = "Skipped";
+      if (IsPatchObsolete(patch))
+        reason = "Obsolete";
+      if (!IsPatchForGameVersion(patch))
+        reason = "Non compatible";
+      return reason;
+    }
+    
+    public static bool IsPatchObsolete(IPatch patch) {
+      var obsolete = patch.GetType().GetCustomAttribute<PatchObsoleteAttribute>();
+      var gameVersion = CommunityPatchSubModule.GameVersion;
+      return obsolete != null && VersionComparer.Compare(gameVersion, obsolete.Version) >= 0;
+    }
+    
+    public static bool IsPatchForGameVersion(IPatch patch) {
+      var notBefore = patch.GetType().GetCustomAttribute<PatchNotBeforeAttribute>();
+      var gameVersion = CommunityPatchSubModule.GameVersion;
+      return !(notBefore != null && VersionComparer.Compare(gameVersion, notBefore.Version) < 0);
+    }
+    
     public static bool IsTargetPatchable(MethodInfo targetMethodInfo, byte[][] targetHashes) {
       if (targetMethodInfo == null)
         return false;
